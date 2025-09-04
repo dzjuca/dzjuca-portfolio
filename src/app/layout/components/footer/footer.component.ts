39aff1e36e3833bef60e52d023981490cdf1fbe5
Daniel Zabrano Juca <dzjuca@gmail.com>
2025-09-04 16:51:53 -0500

    feat(layout): Build and style Footer component

--- src/app/layout/components/footer/footer.component.ts
+++ src/app/layout/components/footer/footer.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
+
+  public currentYear: number = new Date().getFullYear();
 
 }
