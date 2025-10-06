312e01bdb773542803fc05ff3b45504d68826189
Daniel Zabrano Juca <dzjuca@gmail.com>
2025-10-06 07:32:29 -0500

    refactor(security): Implement environment variables for EmailJS keys

---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  emailjs: {
+    publicKey: 'g5dIHgki4KkAKGmMl',
+    serviceId: 'service_5ck10br',
+    templateId: 'template_ror8knj',
+  }
+};
